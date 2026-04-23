c77ca39bf5ac068bfb8a3e73dc32dccf2d3f288f
Inaros Lemuel <infromke@gmail.com>
2026-04-23 01:04:21 -0300

    refactor: simplify passwordResetGuard logic

--- frontend/src/app/core/guards/password-reset.guard.ts
+++ frontend/src/app/core/guards/password-reset.guard.ts
@@ -2,39 +2,37 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 import { UserService } from '../services/user-service';
+import { AuthService } from '../services/auth-service';
+import { catchError, map, of } from 'rxjs';
 
-const passwordResetGuard: CanActivateFn = (route, state) => {
+const passwordResetGuard: CanActivateFn = () => {
   const userService = inject(UserService);
+  const authService = inject(AuthService);
+
   const router = inject(Router);
 
-  const user = userService.userData();
   const resetEmail = userService.resetEmail();
 
-  // usuários logados NÃO acessam o fluxo de forgot-password
-  if (user) {
-    router.navigate(['/home']);
-    return false;
-  }
-
-  const currentPath = state.url;
-
-  // se o dado "email" estiver no state, o usuário pode ir para a página de reset
-  if (
-    resetEmail &&
-    (currentPath === '/forgot-password' || currentPath === '/forgot-password/verify')
-  ) {
-    // o back-end irá validar se o cookie passwordToken existe/é válido
-    router.navigate(['/forgot-password/reset']);
-    return false;
-  }
-
-  // proteção para a página de redefinição de senha
-  if (currentPath === '/forgot-password/reset' && !resetEmail) {
+  // leva o usuário para a primeira etapa do fluxo caso não haja e-mail no state
+  if (!resetEmail) {
     router.navigate(['/forgot-password']);
     return false;
   }
 
-  return true;
+  // verifica se o cookie passwordToken é válido
+  return authService.checkResetStatus().pipe(
+    map((res) => {
+      if (res.active) {
+        return true; // token válido
+      }
+      router.navigate(['/forgot-password']);
+      return false;
+    }),
+    catchError(() => {
+      router.navigate(['/forgot-password']);
+      return of(false); // sessão inválida
+    }),
+  );
 };
 
 export default passwordResetGuard;
