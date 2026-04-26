b37bc583d2d706321e36b1089a570851b89a3606
Inaros Lemuel <infromke@gmail.com>
2026-04-25 23:36:47 -0300

    feat: update login route to use Zod validation schema

--- backend/src/modules/session/session.route.js
+++ backend/src/modules/session/session.route.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 
 import sessionController from './session.controller.js'
-import loginValidator from './session.validator.js'
+import handleValidation from '../../middlewares/handleValidation.js'
+import loginSchema from './session.schema.js'
 import verifyAccessToken from '../../middlewares/verifyAccessToken.js'
 
 const router = Router()
@@ -9,7 +10,7 @@ const router = Router()
 //  --- PUBLIC ROUTES ---
 
 // @route POST /sessions/login
-router.post('/login', loginValidator, sessionController.authenticate)
+router.post('/login', handleValidation(loginSchema), sessionController.authenticate)
 
 // @route POST /sessions/logout
 router.post('/logout', sessionController.terminate)
