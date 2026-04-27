56f8963053a079413783f234f5e3108571aba9de
Inaros Lemuel <infromke@gmail.com>
2026-04-26 23:02:05 -0300

    feat: refactor MongoDB ID verification with Zod

---
+++ backend/src/utils/common.schema.js
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+import mongoose from 'mongoose'
+
+/**
+ * Verifica se o `id` fornecido segue o padrão de ID auto-gerado pelo MongoDB.
+ */
+const idSchema = z.string().refine((val) => mongoose.Types.ObjectId.isValid(val), {
+  message: 'The provided ID format is invalid.',
+})
+
+/**
+ * Verifica a chave `params` e valida se dentro dela existe um `id` que corresponda ao `idSchema`.
+ */
+const paramsIdSchema = z.object({
+  params: z.object({
+    id: idSchema,
+  }),
+})
+
+export { idSchema, paramsIdSchema }
