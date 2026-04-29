791022f13c5ac868d6830306fdc625adb22c0003
Inaros Lemuel <infromke@gmail.com>
2026-04-28 22:16:19 -0300

    refactor: remove try-catch blocks in session controller

--- backend/src/modules/session/session.controller.js
+++ backend/src/modules/session/session.controller.js
@@ -11,48 +11,36 @@ class SessionController {
   status = async (req, res, next) => {
     const { id } = req.user
 
-    try {
-      const user = await this.#sessionService.showStatus(id)
-      return res.status(200).json(user)
-    } catch (error) {
-      next(error)
-    }
+    const user = await this.#sessionService.showStatus(id)
+    return res.status(200).json(user)
   }
 
   login = async (req, res, next) => {
     const { email, password } = req.body
 
-    try {
-      const capsule = await this.#sessionService.authenticate(password, { email })
-      const { formattedUser, accessToken } = capsule
+    const capsule = await this.#sessionService.authenticate(password, { email })
+    const { formattedUser, accessToken } = capsule
 
-      res.cookie('accessToken', accessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // usar TRUE em HTTPS
-        sameSite: 'Lax',
-        maxAge: 24 * 60 * 60 * 1000, // 1 dia
-      })
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // usar TRUE em HTTPS
+      sameSite: 'Lax',
+      maxAge: 24 * 60 * 60 * 1000, // 1 dia
+    })
 
-      return res.status(200).json(formattedUser)
-    } catch (error) {
-      next(error)
-    }
+    return res.status(200).json(formattedUser)
   }
 
   logout = async (req, res, next) => {
-    try {
-      this.#sessionService.terminate(req.user.id)
+    this.#sessionService.terminate(req.user.id)
 
-      res.clearCookie('accessToken', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // usar TRUE em HTTPS
-        sameSite: 'Lax',
-      })
+    res.clearCookie('accessToken', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // usar TRUE em HTTPS
+      sameSite: 'Lax',
+    })
 
-      return res.status(200).json({ message: 'User has logged out.' })
-    } catch (error) {
-      next(error)
-    }
+    return res.status(200).json({ message: 'User has logged out.' })
   }
 }
 
