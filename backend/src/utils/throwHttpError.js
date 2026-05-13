976186dda41203a90ff8ea3a24fd46a6feee85b0
Inaros Lemuel <infromke@gmail.com>
2026-05-13 18:47:17 -0300

    refactor: change Zod's optional parameter name from "details" to "invalidParams"

--- backend/src/utils/throwHttpError.js
+++ backend/src/utils/throwHttpError.js
@@ -2,13 +2,13 @@
  * Cria um objeto Error com um status HTTP personalizado. Também lança o erro.
  * @param {number} status - O código de status HTTP a ser retornado.
  * @param {string} message - A mensagem de erro. Mapeia para `detail` na RFC.
- * @param {Array} details - (Opcional) Array de erros formatados do Zod. Mapeia para `invalid_params`.
+ * @param {Array} invalidParams - (Opcional) Array de erros formatados do Zod. Mapeia para `errors`.
  */
-const throwHttpError = (status, message, details = null) => {
+const throwHttpError = (status, message, invalidParams = null) => {
   const error = new Error(message)
   error.status = status
 
-  if (details) error.errors = details // para o array de erros do Zod
+  if (invalidParams) error.errors = invalidParams // para o array de erros do Zod
 
   throw error
 }
